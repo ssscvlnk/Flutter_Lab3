7a280a47c34efdc9ba2595fe6647cd3462d32739
ssscvlnk <pstartceva@yandex.ru>
2026-04-15 15:08:37 +0300

    Add dice image state and rollDice method

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -6,11 +6,15 @@ const endAlignment = Alignment.bottomCenter;
 
 class GradientContainer
     extends StatelessWidget {
-  const GradientContainer(this.color1,this.color2,this.color3,{super.key});
+  GradientContainer(this.color1,this.color2,this.color3,{super.key});
   final Color color1;
   final Color color2;
   final Color color3;
-  void rollDice() {}
+  var activeDiceImage = 'assets/images/dice-1.png';
+  void rollDice() {
+    activeDiceImage = 'assets/images/dice-4.png';
+    print('Изменили картинку');
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -25,7 +29,7 @@ class GradientContainer
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Image.asset('assets/images/dice-1.png',
+            Image.asset(activeDiceImage,
         width: 300),
             TextButton(
               onPressed: rollDice,
